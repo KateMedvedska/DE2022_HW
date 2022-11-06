572aa1514fb7675f75379b156535e66ac9e3f3d2
Kateryna Medvedska <k.o.medvedska@gmail.com>
2022-11-06 14:37:48 +0200

    fix problem with airflow

--- lect_07/bin/run_airflow.sh
+++ lect_07/bin/run_airflow.sh
@@ -4,4 +4,7 @@
 # TODO: Change this to the path where airflow directory is located
 # (default is ~/airflow)
 export AIRFLOW_HOME=/Users/k.medvedska/Repositories/DE2022_HW/lect_07/airflow
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+export no_proxy="*"
+
 airflow standalone
